6ca8a4f9a8d62af212453253dc09fdb9baeb6788
David Pardo <david.pardo@cloudhesive.com>
2025-09-02 16:30:40 -0500

    feat: test double quote

--- startup.sh
+++ startup.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+esc() {
+    printf "%s\\n" "$1" | sed -e "s/'/'\"'\"'/g" -e "1s/^/'/" -e "\$s/\$/'/"
+}
+
+quoted_config=$(esc "$LISTMONK_messenger_pinpoint__config")
+
 # Generate config.toml from environment variables
 cat > config.toml << EOF
 log_level="${LISTMONK__log_level:-info}"
@@ -10,7 +16,7 @@ read_timeout = "${LISTMONK_server__read_timeout:-5s}"
 write_timeout = "${LISTMONK_server__write_timeout:-5s}"
 
 [messenger.end_user_messaging]
-config = "${LISTMONK_messenger_pinpoint__config:-{\}}"
+config = "${quoted_config:-{\}}"
 
 # Add other configuration sections as needed for listmonk-messenger
 # This should match the structure expected by your listmonk-messenger application
